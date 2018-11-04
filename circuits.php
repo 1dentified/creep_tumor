66f01a010db6f217de3b345a0686b62abe4f3321
arosenmund <aaron.rosenmund@gmail.com>
2018-11-04 13:20:58 -0600

    Remote Commit Setup

---
+++ circuits.php
@@ -0,0 +1,42 @@
+<!doctype html>
+
+<html lang="en">
+
+    <head>
+        <title>Circuits Status</title>
+
+        <meta charset="utf-8">
+        <meta name="viewport" content=
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <style>
+        .h1 
+    </style>
+    
+    </head>    
+
+    <body style="background-color: #212121;">
+        <h1> Circuits Status </h1>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3"><h1 style="color:white;">Circuit 1</h1></div>
+                <div class="col-md-3"><span></span></div>
+                <div class="col-md-3"><h1>Circuit 2</h1></div>
+            </div>
+            <div class="row">
+                <div class="col-md-4"><h1>Circuit 1</h1></div>
+                <div class="col-md-4"><span></span></div>
+                <div class="col-md-4">1</div>
+            </div>
+            <div class="row">
+                <div class="col-md-4"><h1>Circuit 1</h1></div>
+                <div class="col-md-4"><span></span></div>
+                <div class="col-md-4">1</div>
+            </div>
+        </div>    
+    </body>
+
+</html>
